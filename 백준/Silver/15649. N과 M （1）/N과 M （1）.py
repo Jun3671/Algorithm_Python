ab5061e58b648005df15edc8f18c109620cc8f67
준혁 <160326131+Jun3671@users.noreply.github.com>
2025-01-15 17:06:58 +0900

    [Silver III] Title: N과 M (1), Time: 248 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,18 @@
+from itertools import permutations
+
+n,m = map(int , input().split())
+
+arr = []
+
+def back():
+    if len(arr) == m:
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+    for i in range (1 , n+1):
+        if i not in arr:
+            arr.append(i)
+            back()
+            arr.pop()
+back()
